3f28036f708be8899b7447a33637bc0c9afc3eeb
Glitch (hello-webpage) <none>
2019-06-28 21:04:39 +0000

    🐽🔨 Checkpoint ./script.js:378534/1528

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ ctx.width = canvas.width = 400
 ctx.height = canvas.height = 400
 
 let tri = 'F-G-G'
-let incr = 10
 let angle = 0
-let tx = 5
-let ty = 5
+let tx = 30
+let ty = 30
 
 const rules = {
   F: 'F-G+F+G-F',
@@ -15,7 +14,7 @@ const rules = {
 }
 
 function init() {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 7; i++) {
     tri.split('').map(t => {
       if (rules[t]) {
         tri += rules[t]  
@@ -34,22 +33,21 @@ function render() {
         angle -= 120
         ctx.translate(tx, ty)
         ctx.rotate(angle * (Math.PI / 180))
-        ctx.translate(-tx, ty)
         break
       case '+':
         // left
         angle += 120
         ctx.translate(tx, ty)
-        ctx.rotate(angle * (Math.PI / 180))
+        ctx.rotate(-angle * (Math.PI / 180))
         break
       default:
-        angle = 0
+        angle = 120
         ctx.translate(tx, ty)
         ctx.rotate(angle * (Math.PI / 180))
         ctx.lineTo(tx, ty)
         ctx.stroke()
         break
-    }
+    } 
   })
 }
 

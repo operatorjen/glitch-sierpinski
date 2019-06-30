4017355612876d64fdfe07754ea114af63c46f30
Glitch (hello-webpage) <none>
2019-06-30 13:44:27 +0000

    🖇🍃 Checkpoint ./script.js:378534/2422

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
 const canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
-ctx.width = canvas.width = 400
-ctx.height = canvas.height = 400
+ctx.width = canvas.width = window.innerWidth
+ctx.height = canvas.height = window.innerHeight
 
-let tri = 'F-G-G'
+let tri = ['F','-','G','-','G']
 let angle = 0
 let tx = 30
 let ty = 30
 
 const rules = {
-  F: 'F-G+F+G-F',
-  G: 'GG'
+  F: ['F','-','G','+','F','+','G','-','F'],
+  G: ['G','G']
 }
 
-function init() {
-  for (let i = 0; i < 3; i++) {
-    tri.split('').map(t => {
-      if (rules[t]) {
-        tri += rules[t]  
-      } else {
-        tri += t
-      }
-    })
+function getNext() {
+  if (rules[tri[0]]) {
+    tri = tri.concat(rules[tri[0]])
+  } else {
+    tri = tri.concat(tri[0])
+  }
+  
+  plot(tri[0])
+  tri.shift()
+}
+
+ctx.strokeStyle = 'rgba(20, 200, 230, 0.01)'
+
+function plot(t) {
+  switch (t) {
+    case '-':
+      // right
+      angle -= 120
+      //ctx.translate(tx, ty)
+      ctx.rotate(angle * (Math.PI / 180))
+      break
+    case '+':
+      // left
+      angle += 120
+      //ctx.translate(tx, ty)
+      ctx.rotate(-angle * (Math.PI / 180))
+      break
+    default:
+      angle = 120
+      ctx.translate(tx, ty)
+      ctx.rotate(angle * (Math.PI / 180))
+      ctx.lineTo(tx, ty)
+      ctx.stroke()
+      break
   }
 }
 
 ctx.beginPath()
 
 function render() {
-  tri.split('').map(t => {
-    switch (t) {
-      case '-':
-        // right
-        angle -= 120
-        ctx.translate(tx, ty)
-        ctx.rotate(angle * (Math.PI / 180))
-        break
-      case '+':
-        // left
-        angle += 120
-        ctx.translate(tx, ty)
-        ctx.rotate(-angle * (Math.PI / 180))
-        break
-      default:
-        angle = 120
-        ctx.translate(tx, ty)
-        ctx.rotate(angle * (Math.PI / 180))
-        ctx.lineTo(tx, ty)
-        ctx.stroke()
-        break
-    } 
-  })
+  getNext()
+  window.requestAnimationFrame(render)
 }
 
-init()
-ctx.translate(10, 20)
+ctx.translate(110, 20)
 ctx.moveTo(0, 0)
 render()
